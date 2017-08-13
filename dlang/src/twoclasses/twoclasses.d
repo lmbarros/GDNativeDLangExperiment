b14c9578161270c5a3e3f6a4adc9ec2467cf35ed
Leandro Motta Barros <lmb@stackedboxes.org>
2017-08-13 01:19:56 -0300

    Add Godot project and `SpriteMover` "native class"

--- dlang/src/twoclasses/twoclasses.d
+++ dlang/src/twoclasses/twoclasses.d
@@ -1,6 +1,7 @@
 import std.stdio;
 import godot;
 import godot.object;
+import godot.sprite;
 
 
 /**
@@ -26,33 +27,59 @@ public:
     }
 
     /// Scales a given value by the current scale factor.
-	@Method
-	double scale(double value)
-	{
-		return value * _scaleFactor;
-	}
+    @Method
+    double scale(double value)
+    {
+        return value * _scaleFactor;
+    }
 
     /// Ditto
-	@Method
-	godot.Vector2 scale(godot.Vector2 vec)
-	{
-		return vec * _scaleFactor;
-	}
+    @Method
+    godot.Vector2 scale(godot.Vector2 vec)
+    {
+        return vec * _scaleFactor;
+    }
 
     /// Ditto
-	@Method
-	godot.Vector3 scale(godot.Vector3 vec)
-	{
-		return vec * _scaleFactor;
-	}
+    @Method
+    godot.Vector3 scale(godot.Vector3 vec)
+    {
+        return vec * _scaleFactor;
+    }
 
 private:
     double _scaleFactor = 1.0;
 }
 
+/**
+ * Scales things passed to it.
+ *
+ * Quite useless, but works even with vectors!
+ */
+class SpriteMover: GodotScript!Sprite
+{
+public:
+    /// Move the sprite as the player requests.
+    @Method
+    void _process(float delta)
+    {
+        import godot.input;
+        import godot.core.vector2;
+
+        // Could use `alias owner this;` to emulate inheritance and avoid these
+        // explicit references to `owner`, but nah.
+        if (Input.is_action_pressed(String("ui_left")))
+            owner.set_position(owner.get_position() - Vector2(10.0, 0.0));
+        else if (Input.is_action_pressed(String("ui_right")))
+            owner.set_position(owner.get_position() + Vector2(10.0, 0.0));
+    }
+}
+
+
 mixin GodotNativeInit!
 (
     Scaler,
+    SpriteMover,
     (){ writeln("Initializing twoclasses..."); }
 );
 
